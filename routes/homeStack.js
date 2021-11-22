f95011475d4b82aec5c9a395acdb8eccbea24873
Mohsin Miah <moemia12@gmail.com>
2021-11-22 19:00:06 +0000

    Update navigation header

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -3,19 +3,20 @@ import { createStackNavigator } from "react-navigation-stack";
 import PokemonList from "../components/PokemonList";
 import Pokemon from '../components/Pokemon'
 import TopBar from '../shared/TopBar';
+import TopBarBackButton from '../shared/TopBarBackButton';
 import React from 'react';
 
 const screens = {
     Home: {
         screen: PokemonList,
         navigationOptions:{
-            header: () => <TopBar/>
+            header: TopBar
         }
     },
     Pokemon: {
         screen: Pokemon,
         navigationOptions:{
-            header: () => <TopBar/>
+            header: TopBarBackButton
         }
     }
 }
